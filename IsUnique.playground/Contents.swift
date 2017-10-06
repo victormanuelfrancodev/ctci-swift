3abb7a3b5becf97892d75b65fa7023ce2ecf7e62
Safaryari <afani1997@gmail.com>
2017-10-06 06:37:13 -0400

    Add IsUnique.

---
+++ IsUnique.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// With extra data structure
+func isUnique(s: String) -> Bool {
+    guard !s.isEmpty else {
+        return false
+    }
+    return s.count == Set(s.characters).count
+}
+
+isUnique(s: "abc") // returns 'true'
+isUnique(s: "Hello, playground") // returns 'false'
+
+// Without extra data structure
+func isUnique(str: String) -> Bool {
+    
+    return false 
+}
