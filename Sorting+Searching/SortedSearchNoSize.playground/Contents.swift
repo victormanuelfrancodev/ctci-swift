7b0bdf2e352f7c42604ad5c3601d74aeac69eb31
Safaryari <afani1997@gmail.com>
2017-11-01 11:20:27 -0400

    Update SortedSearchNoSize.

--- Sorting+Searching/SortedSearchNoSize.playground/Contents.swift
+++ Sorting+Searching/SortedSearchNoSize.playground/Contents.swift
@@ -2,12 +2,24 @@
 
 import UIKit
 
-var arr: [Int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-func element(at index: Int) -> Int {
-    return !arr.contains(index) ? -1 : arr[index]
+extension Collection {
+    /// Returns the element at the specified index iff it is within bounds, otherwise nil.
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
 }
 
-let indexOfFour = element(at: 3) // returns '4'
-let indexOfTwelve = element(at: 12) // returns '-1'
+var arr: [Int] = [1, 2, 3, 4, 5, 7, 8, 9, 10]
+
+func element(at index: Int) -> Int {
+    if let elem = arr[safe: index] {
+        return elem
+    } else {
+        return -1
+    }
+}
+
+let index = element(at: 3) // returns '4'
+let twelve = element(at: 12) // returns '-1'
+let eight = element(at: 6) // returns '8'
 
