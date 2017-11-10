716f9332b54314ea29405b12b736825b5c54c32a
Safaryari <afani1997@gmail.com>
2017-11-10 17:31:58 -0500

    Update PowerSet.

--- Recursion+DP/PowerSet.playground/Contents.swift
+++ Recursion+DP/PowerSet.playground/Contents.swift
@@ -2,20 +2,12 @@
 
 import UIKit
 
-func power<T>(set: [T]) -> [String] {
-    var powerSet = [""]
-    for s in set { powerSet.append("\(s)") }
-    var combined = ""
-    for s in set { combined += "\(s)" }
-    powerSet.append(combined)
-    for i in 0..<set.count {
-        for j in 0..<set.count {
-            guard i != j else { break }
-            let s = String("\(set[i])\(set[j])".sorted())
-            if !powerSet.contains(s) { powerSet.append(s) }
-         }
+extension Array {
+    var powerSet: [[Element]] {
+        guard !isEmpty else { return [[]] }
+        return Array(self[1...]).powerSet.flatMap { [$0, [self[0]] + $0] }
     }
-    return powerSet
 }
 
-let pS = power(set: [1, 2, 3]) // returns '["", "1", "2", "3", "123", "12", "13", "23"]'
+let pS = [1, 2, 3].powerSet // returns '[[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]'
+
