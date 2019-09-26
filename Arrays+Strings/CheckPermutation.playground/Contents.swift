8030a0aded8d2d7180d356d065ed55ed07e8035b
Victor Manuel Lagunas Franco <victormanuelfrancodev@gmail.com>
2019-09-26 10:00:02 +0900

    "pale" "bale"
    Accept "pale"->"bale" in the book one requirement is this.

--- Arrays+Strings/CheckPermutation.playground/Contents.swift
+++ Arrays+Strings/CheckPermutation.playground/Contents.swift
@@ -7,7 +7,12 @@ func checkPermutation(a: String, b: String) -> Bool {
     if a.count == b.count {
         let aSorted = a.sorted()
         let bSorted = b.sorted()
-        return aSorted == bSorted
+        for x in 0..<a.count{
+         if aSorted.contains(bSorted[x]){
+                counter += 1
+             }
+        }
+        return aSorted.count - 1 == counter
     } else {
         let biggerOne = a.count > b.count ? a.sorted() : b.sorted()
         let smallerOne = a.count > b.count ? b.sorted() : a.sorted()
@@ -24,3 +29,5 @@ func checkPermutation(a: String, b: String) -> Bool {
 checkPermutation(a: "abc", b: "cba") // returns 'true'
 checkPermutation(a: "abcd", b: "adb") // returns 'true'
 checkPermutation(a: "abcf", b: "abd") // returns 'false'
+checkPermutation(a: "pale", b: "bale") // returns 'true'
+checkPermutation(a: "pcle", b: "bale") // returns 'false'
